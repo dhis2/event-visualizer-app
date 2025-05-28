d8dccd4dc0a29aa1a55c1a655e455b6b7f9e4ae1
HendrikThePendric <hendrik@dhis2.org>
2025-05-28 14:27:11 +0200

    chore: add some utility types

---
+++ types/pick-with-field-filters.ts
@@ -0,0 +1,127 @@
+// Copied from:
+// https://github.com/dhis2/maintenance-app-beta/blob/fix/pick-with-field-filter-support-transformers/src/types/generated/pickWithFieldFilters.ts
+import type { BaseIdentifiableObject } from './dhis2-openapi-schemas'
+import type { Prettify } from './utility'
+
+type RemoveSpaces<S extends string> = S extends `${infer T} ${infer U}`
+    ? RemoveSpaces<`${T}${U}`>
+    : S
+
+// utility type to convert a union to an intersection
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UnionToIntersection<U> = (U extends any ? (x: U) => void : never) extends (
+    x: infer I
+) => void
+    ? I
+    : never
+
+// utility type to get the base type of a model, so get an object we can pick from
+//  if in an array, return the model in the array
+//  remove undefined and null modifiers, because we cannot pick if those are present
+type GetModel<T> = NonNullable<T extends Array<infer U> ? U : T>
+
+// we cannot pick within collections, so we keep the original model if it is an array
+// then we wrap in this helper to get the array-type back
+// use extract to keep modifiers like undefined | null
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type MaybeCollection<Model, FullModel> = FullModel extends Array<infer U>
+    ? Model[] | Extract<FullModel, undefined | null>
+    : Model | Extract<FullModel, undefined | null>
+
+type Rename<T, K extends keyof T, N extends string> = Pick<
+    T,
+    Exclude<keyof T, K>
+> & { [P in N]: T[K] }
+
+type IsFieldTransformer<S> = S extends `${string}~size`
+    ? true
+    : S extends `${string}~rename(${string})`
+    ? true
+    : false
+
+type ResolveTransformer<Model, S> =
+    S extends `${infer FieldName extends string &
+        keyof Model}~rename(${infer NewName extends string})${infer Rest}`
+        ? ResolveTransformer<
+              Pick<Rename<Model, FieldName, NewName>, NewName>,
+              `${NewName}${Rest}`
+          >
+        : S extends `${infer FieldName extends string &
+              keyof Model}~size${infer Rest}`
+        ? ResolveTransformer<
+              { [K in FieldName]: number },
+              `${FieldName}${Rest}`
+          >
+        : Model
+
+type SplitNested<S> = S extends `${infer Nested}],${infer Rest}`
+    ? [`${Nested}]`, Rest]
+    : [S]
+
+type RecursivePickWithFieldFilter<
+    Model,
+    S extends string,
+    D extends string = ','
+> = string extends S
+    ? Model // if we cant infer the actual value of the string, return full model
+    : S extends ''
+    ? never
+    : S extends `${infer Root extends string & keyof Model}[${infer Nested}]` // we have a nested filter eg. user[access]
+    ? {
+          // create an object with root key, and recursively pick the nested fields
+          [K in Root]: MaybeCollection<
+              Prettify<
+                  PickWithFieldFilters<
+                      GetModel<Model[Root]>,
+                      SplitNested<Nested>
+                  >
+              >,
+              Model[Root]
+          >
+      }
+    : S extends `${infer T extends string & keyof Model}${D}${infer U}` // simple filter eg. id,name
+    ? Pick<Model, T> & RecursivePickWithFieldFilter<Model, U>
+    : S extends keyof Model
+    ? Pick<Model, S>
+    : S extends ':owner'
+    ? BaseIdentifiableObject
+    : IsFieldTransformer<S> extends true
+    ? ResolveTransformer<Model, S>
+    : never
+// : never //[Model, S] // not a key of Model, ignore
+
+/**
+ * From Model, Pick properties (and nested properties) using a field filter-array.
+ * Field-filters are of the type ['name', 'id', 'access[read,delete]' and can be as deep as needed, to pick the nested fields.
+ *
+ * @example
+ * type DataElementWithCategoryCombo = PickWithFieldFilters<DataElement, ['id', 'name', 'categoryCombo[id,displayName,categories[id,displayName]]']>
+ * const de = {} as DataElementWithCategoryCombo
+ * de.categoryCombo.categories.map(c => c.displayName)
+ **/
+
+export type PickWithFieldFilters<
+    TModel,
+    TFieldFilters extends readonly string[]
+> =
+    // RecursivePick<TModel, TFieldFilters[number]> will return an union of objects for each entry in fieldFilter-array.
+    // Thus we convert this to an intersection to get one object with all the picked properties.
+    Prettify<
+        UnionToIntersection<
+            RecursivePickWithFieldFilter<
+                TModel,
+                RemoveSpaces<TFieldFilters[number]>
+            >
+        >
+    >
+
+// type Test = PickWithFieldFilters<
+//     CategoryCombo,
+//     [
+//         'id',
+//         'name',
+//         'categories[id,displayName,code,categoryOptions[id,displayName],attributeValues~size]',
+//         'categoryOptionCombos~size~rename(cocSize)'
+//         // 'categories~size~rename(K)'
+//     ]
+// >
