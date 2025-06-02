75150be183eaf11497cd665c14abddd695467587
HendrikThePendric <hendrik@dhis2.org>
2025-06-02 14:37:27 +0200

    chore: cypress setup WIP

---
+++ cypress.config.ts
@@ -0,0 +1,71 @@
+import fs from 'node:fs'
+import { chromeAllowXSiteCookies } from '@dhis2/cypress-plugins'
+import { defineConfig } from 'cypress'
+import { excludeByVersionTags } from 'cypress/plugins/excludeByVersionTags.ts'
+
+async function setupNodeEvents(on, config) {
+    chromeAllowXSiteCookies(on, config)
+    excludeByVersionTags(on, config)
+
+    // Delete videos for passing tests
+    on('after:spec', (spec, results) => {
+        try {
+            if (results && results.video) {
+                // Do we have failures for any retry attempts?
+                const failures = results.tests.some((test) =>
+                    test.attempts.some((attempt) => attempt.state === 'failed')
+                )
+                if (!failures) {
+                    // delete the video if the spec passed and no tests retried
+                    fs.unlinkSync(results.video)
+                }
+            }
+        } catch (error) {
+            if (error.code === 'ENOENT') {
+                console.log('Video already deleted')
+            } else {
+                throw error
+            }
+        }
+    })
+
+    if (!config.env.dhis2InstanceVersion) {
+        throw new Error(
+            'dhis2InstanceVersion is missing. Check the README for more information.'
+        )
+    }
+
+    return config
+}
+
+module.exports = defineConfig({
+    projectId: 'NEEDED',
+    e2e: {
+        setupNodeEvents,
+        baseUrl: 'http://localhost:3000',
+        specPattern: 'cypress/integration/**/*.cy.js',
+        viewportWidth: 1280,
+        viewportHeight: 800,
+        defaultCommandTimeout: 15000,
+        /* Globally disable test isolation because the test suite
+         * contains many tests with sequential steps */
+        testIsolation: false,
+        // Record video
+        video: true,
+        // Enabled to reduce the risk of out-of-memory issues
+        experimentalMemoryManagement: true,
+        // Set to a low number to reduce the risk of out-of-memory issues
+        numTestsKeptInMemory: 5,
+        /* When allowing 1 retry on CI, the test suite will pass if
+         * it's flaky. And/but we also get to identify flaky tests on the
+         * Cypress Dashboard. */
+        retries: {
+            runMode: 1,
+            openMode: 0,
+        },
+    },
+    env: {
+        dhis2DatatestPrefix: 'dhis2-datavisualizer',
+        networkMode: 'live',
+    },
+})
---
+++ cypress/plugins/exclude-by-version-tags.ts
@@ -0,0 +1,113 @@
+/**
+ * @type {Cypress.PluginConfig}
+ */
+import type { D2Config } from '@dhis2/cli-app-scripts'
+import { tagify } from 'cypress-tags'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import d2config from '../../d2.config.js'
+
+type D2ConfigWithOptionalFields = Partial<D2Config>
+
+/*
+The list of excluded tags returned by getExcludedTags are the tags that cypress will ignore when running the test suite. So if a test is tagged with one of the tags in the excluded list, then that test will not run.
+
+Using excluded tags (instead of included tags) allows for most of the tests to remain untagged and be run against all supported versions of DHIS2.
+
+DHIS2 officially supports the latest 3 released versions of DHIS2. For example: 2.40, 2.41 and 2.42. Dev would then have version 2.41-SNAPSHOT. Therefore, the getExcludedTags calculates the range of tags based on minimum supported version + 3 (2.40, 2.1, 2.42, 2.43-SNAPSHOT)
+
+With the minimum supported version of 2.40, the tags will always contain "40", "41" and "42", but the comparison symbols will depend on the current instance version.
+
+Allowed tag comparisons are ">", ">=", "<", "<="
+*/
+
+const getString = (v: string | number): string =>
+    typeof v === 'number' ? v.toString() : v
+
+const extractMinorVersion = (v: string): number =>
+    v.indexOf('2.') === 0 ? parseInt(v.slice(2, 4)) : parseInt(v.slice(0, 2))
+
+const MIN_DHIS2_VERSION = extractMinorVersion(
+    getString((d2config as D2ConfigWithOptionalFields).minDHIS2Version)
+)
+
+const getInstanceMinorVersion = (dhis2InstanceVersion: string): number => {
+    if (dhis2InstanceVersion.toLowerCase() === 'dev') {
+        return MIN_DHIS2_VERSION + 3
+    }
+
+    return extractMinorVersion(dhis2InstanceVersion)
+}
+
+export const getExcludedTags = (v: string | number) => {
+    const currentInstanceVersion = getInstanceMinorVersion(getString(v))
+
+    if (currentInstanceVersion < MIN_DHIS2_VERSION) {
+        throw new Error(
+            'Instance version is lower than the minimum supported version'
+        )
+    }
+
+    let excludeTags = []
+    if (currentInstanceVersion === MIN_DHIS2_VERSION) {
+        // For example instance = 2.38, MIN = 2.38
+        excludeTags = [
+            `<${currentInstanceVersion}`,
+            `>${currentInstanceVersion}`,
+            `>=${currentInstanceVersion + 1}`,
+            `>${currentInstanceVersion + 1}`,
+            `>=${currentInstanceVersion + 2}`,
+            `>${currentInstanceVersion + 2}`,
+            `>=${currentInstanceVersion + 3}`,
+        ]
+    } else if (currentInstanceVersion === MIN_DHIS2_VERSION + 1) {
+        // For example instance = 2.39, MIN = 2.38
+        excludeTags = [
+            `<=${currentInstanceVersion - 1}`,
+            `<${currentInstanceVersion - 1}`,
+            `<${currentInstanceVersion}`,
+            `>${currentInstanceVersion}`,
+            `>=${currentInstanceVersion + 1}`,
+            `>${currentInstanceVersion + 1}`,
+            `>=${currentInstanceVersion + 2}`,
+        ]
+    } else if (currentInstanceVersion === MIN_DHIS2_VERSION + 2) {
+        // For example instance = 2.40, MIN = 2.38
+        excludeTags = [
+            `<=${currentInstanceVersion - 2}`,
+            `<${currentInstanceVersion - 2}`,
+            `<=${currentInstanceVersion - 1}`,
+            `<${currentInstanceVersion - 1}`,
+            `<${currentInstanceVersion}`,
+            `>${currentInstanceVersion}`,
+            `>=${currentInstanceVersion + 1}`,
+        ]
+    } else {
+        // For example instance = 2.41, MIN = 2.38
+        excludeTags = [
+            `<=${currentInstanceVersion - 3}`,
+            `<${currentInstanceVersion - 3}`,
+            `<=${currentInstanceVersion - 2}`,
+            `<${currentInstanceVersion - 2}`,
+            `<${currentInstanceVersion - 1}`,
+            `<=${currentInstanceVersion - 1}`,
+            `<${currentInstanceVersion}`,
+        ]
+    }
+
+    return excludeTags
+}
+
+export const excludeByVersionTags = (
+    on: Cypress.PluginEvents,
+    config: Cypress.PluginConfigOptions
+) => {
+    const excludedTags = getExcludedTags(config.env.dhis2InstanceVersion)
+
+    console.log('instanceVersion', config.env.dhis2InstanceVersion)
+    console.log('tags to exclude from testing', excludedTags)
+
+    config.env.CYPRESS_EXCLUDE_TAGS = excludedTags.join(',')
+
+    on('file:preprocessor', tagify(config))
+}
