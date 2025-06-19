d5d516c87487057e7c36e5e66d1fa8d4d9aea599
HendrikThePendric <hendrik@dhis2.org>
2025-06-19 13:31:50 +0200

    chore: add vitest-canvas-mock to suppress highcharts error in tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,6 +2,7 @@ import { configure } from '@testing-library/dom'
 import * as matchers from '@testing-library/jest-dom/matchers'
 import { cleanup } from '@testing-library/react'
 import { expect, afterEach } from 'vitest'
+import 'vitest-canvas-mock'
 
 expect.extend(matchers)
 configure({
