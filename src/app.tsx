2b8cb0632b5c035bce36636c058ffd7efa890b01
HendrikThePendric <hendrik@dhis2.org>
2025-07-10 12:58:28 +0200

    chore: add a toggle to show/hide exaples in app
    This helps making the tests pass without having to mock loads

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,17 @@
 import i18n from '@dhis2/d2-i18n'
 import type { MeDto } from '@types'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { AppWrapper } from './app-wrapper'
 import { useSystemSettings } from './app-wrapper/app-cached-data-query-provider'
 import classes from './app.module.css'
+import { DashboardExample } from './components/examples/dashboard-example'
+import { LazyUserProfileExample } from './components/examples/lazy-user-profile-example'
 import { UserProfileExample } from './components/examples/user-profile-example'
 import Hello from './hello'
 import { useRtkQuery } from './hooks'
 
 const EventVisualizer: FC = () => {
+    const [showExamples, setShowExamples] = useState(false)
     const rtkqQuery = useRtkQuery({
         resource: 'me',
     })
@@ -24,15 +27,27 @@ const EventVisualizer: FC = () => {
 
     const me = rtkqQuery.data as MeDto
 
-    console.log('RTKQ Data):', rtkqQuery.data)
     console.log('systemSettings', systemSettings)
 
     return (
         <div className={classes.container}>
             <Hello name={me.name} />
             <h3>{i18n.t('Welcome to DHIS2 with TypeScript!')}</h3>
-            <h4>Example Component:</h4>
-            <UserProfileExample />
+            <button onClick={() => setShowExamples((v) => !v)}>
+                {showExamples ? 'Hide' : 'Show'} Examples
+            </button>
+            {showExamples && (
+                <div>
+                    <h4>User Profile Example:</h4>
+                    <UserProfileExample />
+                    <hr />
+                    <h4>Lazy User Profile Example:</h4>
+                    <LazyUserProfileExample />
+                    <hr />
+                    <h4>Dashboard Example:</h4>
+                    <DashboardExample />
+                </div>
+            )}
         </div>
     )
 }
