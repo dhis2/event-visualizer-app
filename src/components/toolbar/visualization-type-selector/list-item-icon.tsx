38d620a9bef9a29b34f0dbb74df36259e15fc22d
Edoardo Sabadelli <edoardo@dhis2.org>
2025-07-30 09:50:06 +0200

    fix: update ui and use correct icons for PT/LL

--- src/components/toolbar/visualization-type-selector/list-item-icon.tsx
+++ src/components/toolbar/visualization-type-selector/list-item-icon.tsx
@@ -1,7 +1,9 @@
 import { VIS_TYPE_LINE_LIST, VIS_TYPE_PIVOT_TABLE } from '@dhis2/analytics'
-import { IconVisualizationLine24 } from '@dhis2/ui'
+import {
+    IconVisualizationLinelist24,
+    IconVisualizationPivotTable24,
+} from '@dhis2/ui'
 import React, { FC } from 'react'
-import PivotTableIcon from '../../../assets/pivot-table-icon'
 
 type ListItemIconProps = {
     iconType: string
@@ -13,11 +15,15 @@ const ListItemIcon: FC<ListItemIconProps> = ({ iconType, style }) => {
         return (
             <div style={style}>
                 {/* TODO replace with the proper colored icon from assets */}
-                <IconVisualizationLine24 />
+                <IconVisualizationLinelist24 />
             </div>
         )
     } else if (iconType === VIS_TYPE_PIVOT_TABLE) {
-        return <PivotTableIcon style={style} />
+        return (
+            <div style={style}>
+                <IconVisualizationPivotTable24 />
+            </div>
+        )
     }
 }
 
