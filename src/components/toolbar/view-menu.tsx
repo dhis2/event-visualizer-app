0f4cb59ac553789c16bd49e1b0821c57bc763681
Edoardo Sabadelli <edoardo@dhis2.org>
2025-07-31 16:30:37 +0200

    feat: add view menu in toolbar

---
+++ src/components/toolbar/view-menu.tsx
@@ -0,0 +1,94 @@
+import {
+    HoverMenuDropdown,
+    HoverMenuList,
+    HoverMenuListItem,
+} from '@dhis2/analytics'
+import i18n from '@dhis2/d2-i18n'
+import React /*useCallback*/ from 'react'
+//import { useDispatch, useSelector } from 'react-redux'
+//import {
+//    acToggleUiSidebarHidden,
+//    acToggleUiLayoutPanelHidden,
+//    acSetUiDetailsPanelOpen,
+//    acSetUiAccessoryPanelWidth,
+//} from '../../actions/ui.js'
+//import { ACCESSORY_PANEL_DEFAULT_WIDTH } from '../../modules/accessoryPanelConstants.js'
+//import { setUserSidebarWidthToLocalStorage } from '../../modules/localStorage.js'
+//import { sGetCurrentId } from '../../reducers/current.js'
+//import {
+//    sGetUiLayoutPanelHidden,
+//    sGetUiSidebarHidden,
+//    sGetUiShowDetailsPanel,
+//    sGetUiAccessoryPanelWidth,
+//} from '../../reducers/ui.js'
+
+const ViewMenu = () => {
+    //    const dispatch = useDispatch()
+    const isSidebarHidden = false //useSelector(sGetUiSidebarHidden)
+    const isLayoutPanelHidden = false // useSelector(sGetUiLayoutPanelHidden)
+    const isDetailsPanelOpen = false // useSelector(sGetUiShowDetailsPanel)
+    //    const userSettingWidth = useSelector(sGetUiAccessoryPanelWidth)
+    //    const id = useSelector(sGetCurrentId)
+
+    const TBD = () => console.log('TBD')
+
+    const toggleLayoutPanelHidden = TBD
+    //useCallback(() => {
+    //        dispatch(acToggleUiLayoutPanelHidden())
+    //}, [dispatch])
+
+    const toggleSidebarHidden = TBD
+    //useCallback(() => {
+    //        dispatch(acToggleUiSidebarHidden())
+    //}, [dispatch])
+
+    const resetAccessorySidebarWidth = TBD
+    //useCallback(() => {
+    //        setUserSidebarWidthToLocalStorage(ACCESSORY_PANEL_DEFAULT_WIDTH)
+    //        dispatch(acSetUiAccessoryPanelWidth(ACCESSORY_PANEL_DEFAULT_WIDTH))
+    //}, [dispatch])
+
+    const toggleDetailsPanelOpen = TBD
+    //useCallback(() => {
+    //        dispatch(acSetUiDetailsPanelOpen(!isDetailsPanelOpen))
+    //}, [dispatch, isDetailsPanelOpen])
+
+    const toggleLayoutPanelText = isLayoutPanelHidden
+        ? i18n.t('Show layout')
+        : i18n.t('Hide layout')
+    const toggleSidebarText = isSidebarHidden
+        ? i18n.t('Show dimensions sidebar')
+        : i18n.t('Hide dimensions sidebar')
+    const toggleDetailsPanelText = isDetailsPanelOpen
+        ? i18n.t('Hide interpretations and details')
+        : i18n.t('Show interpretations and details')
+
+    return (
+        <HoverMenuDropdown label={i18n.t('View')}>
+            <HoverMenuList>
+                <HoverMenuListItem
+                    label={toggleLayoutPanelText}
+                    onClick={toggleLayoutPanelHidden}
+                />
+                <HoverMenuListItem
+                    label={toggleSidebarText}
+                    onClick={toggleSidebarHidden}
+                />
+                <HoverMenuListItem
+                    label={i18n.t('Reset sidebar width')}
+                    onClick={resetAccessorySidebarWidth}
+                    //                    disabled={
+                    //                        userSettingWidth === ACCESSORY_PANEL_DEFAULT_WIDTH
+                    //                    }
+                />
+                <HoverMenuListItem
+                    label={toggleDetailsPanelText}
+                    onClick={toggleDetailsPanelOpen}
+                    //                    disabled={!id}
+                />
+            </HoverMenuList>
+        </HoverMenuDropdown>
+    )
+}
+
+export default ViewMenu
