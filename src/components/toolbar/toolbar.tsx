f847b1b6ede681f4d730360d343631327b42653e
Edoardo Sabadelli <edoardo@dhis2.org>
2025-08-05 14:35:02 +0200

    chore: add types for analytics components

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,8 +1,8 @@
 import { Toolbar as AnalyticsToolbar } from '@dhis2/analytics'
-import React from 'react'
+import React, { FC } from 'react'
 import VisualizationTypeSelector from './visualization-type-selector/visualization-type-selector'
 
-const Toolbar = () => (
+const Toolbar: FC = () => (
     <AnalyticsToolbar>
         <VisualizationTypeSelector />
     </AnalyticsToolbar>
--- src/types/analytics/index.d.ts
+++ src/types/analytics/index.d.ts
@@ -3,7 +3,12 @@ import type {
     useCachedDataQuery,
 } from './cached-data-query-provider'
 
+import type { Toolbar } from './toolbar'
+import type { ToolbarSidebar } from './toolbar-sidebar'
+
 declare module '@dhis2/analytics' {
     export const CachedDataQueryProvider: CachedDataQueryProvider
     export const useCachedDataQuery: useCachedDataQuery
+    export const Toolbar: Toolbar
+    export const ToolbarSidebar: ToolbarSidebar
 }
