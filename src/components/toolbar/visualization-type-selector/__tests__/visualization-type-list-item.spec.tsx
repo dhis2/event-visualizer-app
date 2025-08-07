ebfbcef6d73835d85ff7081f4ed5385faf5e05e2
Edoardo Sabadelli <edoardo@dhis2.org>
2025-08-07 15:23:50 +0200

    refactor: do not use vis type from analytics
    Also ensure the type of iconType is correct

--- src/components/toolbar/visualization-type-selector/__tests__/visualization-type-list-item.spec.tsx
+++ src/components/toolbar/visualization-type-selector/__tests__/visualization-type-list-item.spec.tsx
@@ -1,4 +1,3 @@
-import { VIS_TYPE_LINE_LIST } from '@dhis2/analytics'
 import { render } from '@testing-library/react'
 import React from 'react'
 import { vi } from 'vitest'
@@ -6,13 +5,13 @@ import { VisualizationTypeListItem } from '../visualization-type-list-item'
 
 test('VisualizationTypeListItem renders Line list item', () => {
     const props = {
-        iconType: VIS_TYPE_LINE_LIST,
+        iconType: 'LINE_LIST',
         label: 'Line list',
         description: 'A visualization that displays data in a line list format',
         disabled: false,
         isSelected: false,
         onClick: vi.fn(),
-    }
+    } as const
 
     const { container } = render(<VisualizationTypeListItem {...props} />)
     expect(container).toMatchSnapshot()
