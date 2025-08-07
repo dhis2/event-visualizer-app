57f7f8f36d684c93b55071738daec6a7f00b3b7e
Edoardo Sabadelli <edoardo@dhis2.org>
2025-08-07 10:44:19 +0200

    chore: add types for analytics components

---
+++ src/types/analytics/hover-menu-list.ts
@@ -0,0 +1,12 @@
+import { FC, ReactNode } from 'react'
+
+type HoverMenuListProps = {
+    children: ReactNode
+    className?: string
+    dataTest?: string
+    dense?: boolean
+    maxHeight?: string
+    maxWidth?: string
+}
+
+export type HoverMenuList = FC<HoverMenuListProps>
