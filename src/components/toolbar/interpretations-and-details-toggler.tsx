593587e2bc66bf83aa495adf1d9b290da5246ae2
Edoardo Sabadelli <edoardo@dhis2.org>
2025-08-19 15:53:59 +0200

    feat: connect interpretations/details toggler to the store

--- src/components/toolbar/interpretations-and-details-toggler.tsx
+++ src/components/toolbar/interpretations-and-details-toggler.tsx
@@ -1,16 +1,31 @@
 import type { FC } from 'react'
+import { useCallback } from 'react'
 import { InterpretationsAndDetailsToggler as AnalyticsInterpretationsAndDetailsToggler } from '@dhis2/analytics'
+import { useAppSelector, useAppDispatch } from '@hooks'
+import { currentSlice } from '@store/current-slice'
+import { uiSlice, setUiDetailsPanelOpen } from '@store/ui-slice'
 
 export const InterpretationsAndDetailsToggler: FC = () => {
-    const showDetailsPanel = false
-    const id = undefined
-    const onClick = () => console.log('TBD')
+    const dispatch = useAppDispatch()
+
+    const { getCurrentId } = currentSlice.selectors
+    const { getUiDetailsPanelOpen } = uiSlice.selectors
+
+    const id = useAppSelector((state) => getCurrentId(state))
+
+    const isDetailsPanelOpen = useAppSelector((state) =>
+        getUiDetailsPanelOpen(state)
+    )
+
+    const onClick = useCallback(() => {
+        dispatch(setUiDetailsPanelOpen(!isDetailsPanelOpen))
+    }, [dispatch, isDetailsPanelOpen])
 
     return (
         <AnalyticsInterpretationsAndDetailsToggler
             disabled={!id}
             onClick={onClick}
-            isShowing={showDetailsPanel}
+            isShowing={isDetailsPanelOpen}
         />
     )
 }
