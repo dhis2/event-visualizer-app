e237994fcdc5fe161ac82f8ae94337f09f7833e2
Edoardo Sabadelli <edoardo@dhis2.org>
2025-08-25 14:13:44 +0200

    feat: add Update button to toolbar

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,11 +1,13 @@
 import type { FC } from 'react'
 import { InterpretationsAndDetailsToggler } from './interpretations-and-details-toggler'
+import { UpdateButton } from './update-button'
 import { VisualizationTypeSelector } from './visualization-type-selector/visualization-type-selector'
 import { Toolbar as AnalyticsToolbar } from '@dhis2/analytics'
 
 export const Toolbar: FC = () => (
     <AnalyticsToolbar>
         <VisualizationTypeSelector />
+        <UpdateButton />
         <InterpretationsAndDetailsToggler />
     </AnalyticsToolbar>
 )
