7cc80584b504e37911f7cd9c123913204b7c4468
Edoardo Sabadelli <edoardo@dhis2.org>
2025-08-26 11:24:07 +0200

    feat: add Update button to toolbar (#32)

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,12 +1,14 @@
 import type { FC } from 'react'
 import { InterpretationsAndDetailsToggler } from './interpretations-and-details-toggler'
 import { MenuBar } from './menu-bar/menu-bar'
+import { UpdateButton } from './update-button'
 import { VisualizationTypeSelector } from './visualization-type-selector/visualization-type-selector'
 import { Toolbar as AnalyticsToolbar } from '@dhis2/analytics'
 
 export const Toolbar: FC = () => (
     <AnalyticsToolbar>
         <VisualizationTypeSelector />
+        <UpdateButton />
         <MenuBar />
         <InterpretationsAndDetailsToggler />
     </AnalyticsToolbar>
--- src/types/analytics/index.d.ts
+++ src/types/analytics/index.d.ts
@@ -9,6 +9,7 @@ import type { HoverMenuListItem } from './hover-menu-list-item'
 import type { InterpretationsAndDetailsToggler } from './interpretations-and-details-toggler'
 import type { Toolbar } from './toolbar'
 import type { ToolbarSidebar } from './toolbar-sidebar'
+import type { UpdateButton } from './update-button'
 import type { EventVisualizationType, VisualizationType } from '@types'
 
 declare module '@dhis2/analytics' {
@@ -21,6 +22,7 @@ declare module '@dhis2/analytics' {
     export const InterpretationsAndDetailsToggler: InterpretationsAndDetailsToggler
     export const Toolbar: Toolbar
     export const ToolbarSidebar: ToolbarSidebar
+    export const UpdateButton: UpdateButton
     export const visTypeDisplayNames: Array<
         Record<EventVisualizationType | VisualizationType, string>
     >
