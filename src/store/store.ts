bbed93e3740d8849f88eed1ea803fed430b240af
Edoardo Sabadelli <edoardo@dhis2.org>
2025-08-28 16:34:00 +0200

    feat: add listener middleware for reacting to store changes

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { currentVisSlice } from './current-vis-slice'
+import { listenerMiddleware } from './middleware/listener'
 import { navigationSlice } from './navigation-slice'
 import { savedVisSlice } from './saved-vis-slice'
 import { uiSlice } from './ui-slice'
@@ -24,7 +25,9 @@ export const createStore = (
                 thunk: {
                     extraArgument: { engine, metadataStore, appChachedData },
                 },
-            }).concat(api.middleware),
+            })
+                .prepend(listenerMiddleware.middleware)
+                .concat(api.middleware),
     })
 }
 
