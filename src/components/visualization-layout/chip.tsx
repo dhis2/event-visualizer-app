524aa8f7c36d54870404eb70b34cfc9f79be23ce
Jen Jones Arnesen <jennifer@dhis2.org>
2025-09-03 15:21:51 +0200

    feat: add chip menu button - not active yet

--- src/components/visualization-layout/chip.tsx
+++ src/components/visualization-layout/chip.tsx
@@ -1,6 +1,8 @@
+import { IconMore16 } from '@dhis2/ui'
 import React from 'react'
 import { ChipBase } from './chip-base'
 import classes from './styles/chip.module.css'
+import { IconButton } from '@components/dimension-item/icon-button'
 import type { SupportedDimensionType } from '@constants/dimension-types'
 
 export interface LayoutDimension {
@@ -25,6 +27,13 @@ export const Chip: React.FC<ChipProps> = ({ dimension }) => {
         <div className={classes.chip} data-test="layout-dimension-chip">
             <div className={classes.content}>
                 <ChipBase dimension={dimension} />
+                <IconButton
+                    onClick={() => console.log('TODO - open menu')}
+                    dataTest={`chip-menu-button-${dimension.id}`}
+                    menuId={`chip-menu-${dimension.id}`}
+                >
+                    <IconMore16 />
+                </IconButton>
             </div>
         </div>
     )
