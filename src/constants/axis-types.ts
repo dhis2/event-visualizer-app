d84d812c3766ec07eeb4f7eda8fd7f687481ce5e
Jen Jones Arnesen <jennifer@dhis2.org>
2025-09-08 16:15:29 +0200

    chore: clean up axis-types

--- src/constants/axis-types.ts
+++ src/constants/axis-types.ts
@@ -1,12 +1,1 @@
-// Individual axis ID constants
-export const AXIS_ID_COLUMNS = 'columns'
-export const AXIS_ID_FILTERS = 'filters'
-export const AXIS_ID_ROWS = 'rows'
-
-export const SUPPORTED_AXIS_IDS = [
-    AXIS_ID_COLUMNS,
-    AXIS_ID_FILTERS,
-    AXIS_ID_ROWS,
-] as const
-
-export type SupportedAxisId = (typeof SUPPORTED_AXIS_IDS)[number]
+export type SupportedAxisId = 'columns' | 'filters' | 'rows'
