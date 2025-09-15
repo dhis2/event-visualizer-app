4e266a79f255af60869e7b5f08a2a28202110d6e
Edoardo Sabadelli <edoardo@dhis2.org>
2025-09-15 13:55:55 +0200

    chore: add TODO comment and reference to ticket

--- src/components/plugin-wrapper/hooks/query-tools-common.ts
+++ src/components/plugin-wrapper/hooks/query-tools-common.ts
@@ -3,6 +3,8 @@ import type { CurrentVisualization, DimensionId } from '@types'
 
 export type ParameterRecord = Record<DimensionId, unknown>
 
+// This should be refactored when the work on Options is done.
+// See: https://dhis2.atlassian.net/browse/DHIS2-19823
 export const getRequestOptions = (visualization: CurrentVisualization) => {
     const options = getOptionsForRequest().reduce(
         (map, [option, props]): ParameterRecord => {
