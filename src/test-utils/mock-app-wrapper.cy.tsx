ea8e53ad60023bb0376826b0079acd8642c98692
HendrikThePendric <hendrik@dhis2.org>
2025-09-30 14:48:09 +0200

    chore: add cypress test for mock-app-wrapper component

---
+++ src/test-utils/mock-app-wrapper.cy.tsx
@@ -0,0 +1,35 @@
+import { MockAppWrapper } from './render-with-app-wrapper'
+import { useMetadataItem } from '@components/app-wrapper/metadata-provider'
+import { useCurrentUser } from '@hooks'
+
+const TestComponent = () => {
+    const currentUser = useCurrentUser()
+    const userOrgUnitMetadata = useMetadataItem('USER_ORGUNIT')
+
+    return (
+        <div>
+            <div data-test="current-user">
+                {currentUser?.username || 'No User'}
+            </div>
+            <div data-test="metadata">
+                {userOrgUnitMetadata?.name || 'No Metadata'}
+            </div>
+        </div>
+    )
+}
+
+describe('MockAppWrapper', () => {
+    it('should provide mocked app context with AppCachedDataQueryProvider and MetadataContext', () => {
+        cy.mount(
+            <MockAppWrapper>
+                <TestComponent />
+            </MockAppWrapper>
+        )
+
+        cy.getByDataTest('current-user').should('contain.text', 'admin')
+        cy.getByDataTest('metadata').should(
+            'contain.text',
+            'User organisation unit'
+        )
+    })
+})
