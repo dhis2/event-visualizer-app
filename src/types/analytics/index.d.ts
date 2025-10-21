70acd7217e482c9a16ee0da42faeb1d3e495526f
Edoardo Sabadelli <edoardo@dhis2.org>
2025-10-21 16:13:29 +0200

    feat: implement DetailsPanel for right sidebar
    This renders the about and interpretations thread components from
    analytics.

--- src/types/analytics/index.d.ts
+++ src/types/analytics/index.d.ts
@@ -1,3 +1,4 @@
+import type { AboutAOUnit } from './about-ao-unit'
 import type {
     CachedDataQueryProvider,
     useCachedDataQuery,
@@ -8,7 +9,10 @@ import type { HoverMenuBar } from './hover-menu-bar'
 import type { HoverMenuDropdown } from './hover-menu-dropdown'
 import type { HoverMenuList } from './hover-menu-list'
 import type { HoverMenuListItem } from './hover-menu-list-item'
+import type { InterpretationModal } from './interpretation-modal'
 import type { InterpretationsAndDetailsToggler } from './interpretations-and-details-toggler'
+import type { InterpretationsProvider } from './interpretations-provider'
+import type { InterpretationsUnit } from './interpretations-unit'
 import type { PivotTable } from './pivot-table'
 import type { Toolbar } from './toolbar'
 import type { ToolbarSidebar } from './toolbar-sidebar'
@@ -25,6 +29,7 @@ import type {
 } from '@types'
 
 declare module '@dhis2/analytics' {
+    export const AboutAOUnit: AboutAOUnit
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     export const Analytics: any
     export const CachedDataQueryProvider: CachedDataQueryProvider
@@ -35,7 +40,10 @@ declare module '@dhis2/analytics' {
     export const HoverMenuDropdown: HoverMenuDropdown
     export const HoverMenuListItem: HoverMenuListItem
     export const HoverMenuList: HoverMenuList
+    export const InterpretationModal: InterpretationModal
     export const InterpretationsAndDetailsToggler: InterpretationsAndDetailsToggler
+    export const InterpretationsProvider: InterpretationsProvider
+    export const InterpretationsUnit: InterpretationsUnit
     export const PivotTable: PivotTable
     export const Toolbar: Toolbar
     export const ToolbarSidebar: ToolbarSidebar
