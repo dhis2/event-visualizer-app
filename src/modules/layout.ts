ae39da6b54e023875c3aa6c522972f57c25338ca
Edoardo Sabadelli <edoardo@dhis2.org>
2025-11-12 10:41:13 +0100

    refactor: add LayoutPanel component (#83)

--- src/modules/layout.ts
+++ src/modules/layout.ts
@@ -1,6 +1,8 @@
 import i18n from '@dhis2/d2-i18n'
 import type { Axis } from '@types'
 
+export const getAxisName = (axisId: Axis): string => getAxisNames()[axisId]
+
 export const getAxisNames = (): Record<Axis, string> => ({
     columns: i18n.t('Columns'),
     filters: i18n.t('Filter'),
