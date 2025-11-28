a8eae6c66a1a76bec3916c17a02c5c762641dea2
Edoardo Sabadelli <edoardo@dhis2.org>
2025-11-28 17:01:30 +0100

    feat: add option sections configuration for LL

--- src/components/options/sections/line-list-legend-section.tsx
+++ src/components/options/sections/line-list-legend-section.tsx
@@ -1,10 +1,9 @@
 import { type FC } from 'react'
+import classes from './styles/sections.module.css'
 import { Legend } from '@components/options/fields/legend'
 
-export const LineListLegendSection: FC = () => {
-    return (
-        <div data-test="line-list-legend-section">
-            <Legend />
-        </div>
-    )
-}
+export const LineListLegendSection: FC = () => (
+    <div className={classes.section} data-test="line-list-legend-section">
+        <Legend />
+    </div>
+)
