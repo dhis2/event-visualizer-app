74fb06c451d9445af4b799c1fd5501dc0207c0ee
Edoardo Sabadelli <edoardo@dhis2.org>
2025-12-01 15:10:01 +0100

    feat: configure Data and Style section options for PT

--- src/components/options/sections/pivot-table-data-section.tsx
+++ src/components/options/sections/pivot-table-data-section.tsx
@@ -1,7 +1,24 @@
+import i18n from '@dhis2/d2-i18n'
 import type { FC } from 'react'
+import classes from './styles/sections.module.css'
+import { ColSubTotals } from '@components/options/fields/col-sub-totals'
+import { ColTotals } from '@components/options/fields/col-totals'
+import { HideEmptyColumns } from '@components/options/fields/hide-empty-columns'
+import { HideEmptyRows } from '@components/options/fields/hide-empty-rows'
+import { HideNaData } from '@components/options/fields/hide-na-data'
+import { RowSubTotals } from '@components/options/fields/row-sub-totals'
+import { RowTotals } from '@components/options/fields/row-totals'
 
 export const PivotTableDataSection: FC = () => (
-    <h2 data-test="pivot-table-data-section">
-        Options for visType PIVOT_TABLE, section data
-    </h2>
+    <div className={classes.section} data-test="pivot-table-data-section">
+        <span className={classes.sectionTitle}>{i18n.t('Totals')}</span>
+        <ColTotals />
+        <ColSubTotals />
+        <RowTotals />
+        <RowSubTotals />
+        <span className={classes.sectionTitle}>{i18n.t('Empty data')}</span>
+        <HideEmptyColumns />
+        <HideEmptyRows />
+        <HideNaData />
+    </div>
 )
--- src/components/options/sections/pivot-table-style-section.tsx
+++ src/components/options/sections/pivot-table-style-section.tsx
@@ -1,7 +1,21 @@
+import i18n from '@dhis2/d2-i18n'
 import type { FC } from 'react'
+import classes from './styles/sections.module.css'
+import { DigitGroupSeparator } from '@components/options/fields/digit-group-separator'
+import { DisplayDensity } from '@components/options/fields/display-density'
+import { FontSize } from '@components/options/fields/font-size'
+import { ShowHierarchy } from '@components/options/fields/show-hierarchy'
+import { Subtitle } from '@components/options/fields/subtitle'
+import { Title } from '@components/options/fields/title'
 
 export const PivotTableStyleSection: FC = () => (
-    <h2 data-test="pivot-table-style-section">
-        Options for visType PIVOT_TABLE, section style
-    </h2>
+    <div className={classes.section} data-test="pivot-table-style-section">
+        <Title label={i18n.t('Table title')} />
+        <Subtitle label={i18n.t('Table subtitle')} />
+        <DisplayDensity />
+        <FontSize />
+        <DigitGroupSeparator />
+        <span className={classes.sectionTitle}>{i18n.t('Labels')}</span>
+        <ShowHierarchy />
+    </div>
 )
