9f6dc6dcaa646c5c4f969d1a7ba4c36426447be9
HendrikThePendric <hendrik@dhis2.org>
2025-12-08 15:54:56 +0100

    chore: refactor chip-menu

--- src/components/layout-panel/chip-menu.tsx
+++ src/components/layout-panel/chip-menu.tsx
@@ -1,6 +1,6 @@
 import i18n from '@dhis2/d2-i18n'
 import { FlyoutMenu, MenuDivider, MenuItem } from '@dhis2/ui'
-import { type FC } from 'react'
+import { useCallback, useMemo, type FC } from 'react'
 import type { LayoutDimension } from './chip'
 import { /* useAppDispatch,*/ useAppSelector } from '@hooks'
 import { getAxisName } from '@modules/layout'
@@ -12,80 +12,75 @@ type ChipMenuProps = {
     dimensionId: LayoutDimension['id']
     onClose: () => void
 }
+const AXIS_IDS: Axis[] = ['columns', 'rows', 'filters']
 
 export const ChipMenu: FC<ChipMenuProps> = ({
     axisId,
     dimensionId,
     onClose,
 }) => {
-    //    const dispatch = useAppDispatch()
+    // const dispatch = useAppDispatch()
     const visType = useAppSelector(getVisUiConfigVisualizationType)
-
     const dataTest = 'chip-menu'
-
-    const axisItemHandler = ({ dimensionId, axisId }) => {
-        //        dispatch(
-        //            acAddUiLayoutDimensions(
-        //                { [dimensionId]: { axisId } },
-        //                dimensionMetadata
-        //            )
-        //        )
-
-        console.log(`TBD: move dimension ${dimensionId} to axis ${axisId}`)
-
-        onClose()
-    }
-
-    const removeItemHandler = (dimensionId) => {
-        //        dispatch(acRemoveUiLayoutDimensions(id))
-
-        console.log(`TBD: remove dimension ${dimensionId} from layout`)
-
-        onClose()
-    }
-
-    const availableAxisIds: Axis[] =
-        visType === 'LINE_LIST'
-            ? ['columns', 'filters']
-            : ['columns', 'rows', 'filters']
-
-    const applicableAxisIds = availableAxisIds.filter(
-        (availableAxisId) => availableAxisId !== axisId
+    const axisItemHandler = useCallback(
+        ({ dimensionId, axisId }: { dimensionId: string; axisId: Axis }) => {
+            // dispatch(
+            //     acAddUiLayoutDimensions(
+            //         { [dimensionId]: { axisId } },
+            //         dimensionMetadata
+            //     )
+            // )
+            console.log(`TBD: move dimension ${dimensionId} to axis ${axisId}`)
+            onClose()
+        },
+        [onClose]
+    )
+    const removeItemHandler = useCallback(
+        (dimensionId: string) => {
+            // dispatch(acRemoveUiLayoutDimensions(id))
+            console.log(`TBD: remove dimension ${dimensionId} from layout`)
+            onClose()
+        },
+        [onClose]
     )
 
-    const menuItems =
-        // Move to [Columns, Rows, Filter] item(s)
-        applicableAxisIds.map((axisId) => (
+    const applicableAxisIds = useMemo<Axis[]>(
+        () =>
+            AXIS_IDS.filter(
+                (axis) =>
+                    axis !== axisId &&
+                    !(axis === 'rows' && visType === 'LINE_LIST')
+            ),
+        [visType, axisId]
+    )
+
+    return (
+        <FlyoutMenu dense>
+            {applicableAxisIds.map((axisId) => (
+                <MenuItem
+                    key={`${dimensionId}-to-${axisId}`}
+                    onClick={() => {
+                        axisItemHandler({
+                            dimensionId,
+                            axisId,
+                        })
+                        onClose()
+                    }}
+                    label={i18n.t(`Move to {{axisName}}`, {
+                        axisName: getAxisName(axisId),
+                    })}
+                    dataTest={`${dataTest}-item-move-${dimensionId}-to-${axisId}`}
+                />
+            ))}
+            {applicableAxisIds.length > 0 && (
+                <MenuDivider key="menu-divider" dense />
+            )}
             <MenuItem
-                key={`${dimensionId}-to-${axisId}`}
-                onClick={() => {
-                    axisItemHandler({
-                        dimensionId,
-                        axisId,
-                    })
-                    onClose()
-                }}
-                label={i18n.t(`Move to {{axisName}}`, {
-                    axisName: getAxisName(axisId),
-                })}
-                dataTest={`${dataTest}-item-move-${dimensionId}-to-${axisId}`}
+                key={`remove-${dimensionId}`}
+                onClick={() => removeItemHandler(dimensionId)}
+                label={i18n.t('Remove')}
+                dataTest={`${dataTest}-item-remove-${dimensionId}`}
             />
-        ))
-
-    // Divider
-    if (applicableAxisIds.length) {
-        menuItems.push(<MenuDivider key="menu-divider" dense />)
-    }
-
-    // Remove item
-    menuItems.push(
-        <MenuItem
-            key={`remove-${dimensionId}`}
-            onClick={() => removeItemHandler(dimensionId)}
-            label={i18n.t('Remove')}
-            dataTest={`${dataTest}-item-remove-${dimensionId}`}
-        />
+        </FlyoutMenu>
     )
-
-    return <FlyoutMenu dense>{menuItems}</FlyoutMenu>
 }
