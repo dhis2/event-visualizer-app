da2acce7deb997d06ab002d7d82016ebce198280
Edoardo Sabadelli <edoardo@dhis2.org>
2025-12-15 11:24:33 +0100

    chore: address PR comments

--- src/store/vis-ui-config-slice.ts
+++ src/store/vis-ui-config-slice.ts
@@ -33,9 +33,9 @@ export const initialState: VisUiConfigState = {
     options: DEFAULT_OPTIONS,
     outputType: 'EVENT',
     layout: {
-        columns: EMPTY_STRING_ARRAY,
-        filters: EMPTY_STRING_ARRAY,
-        rows: EMPTY_STRING_ARRAY,
+        columns: [],
+        filters: [],
+        rows: [],
     },
     itemsByDimension: {},
     conditionsByDimension: {},
