c11349130b724b2f67b639c28ba4972f023ade51
HendrikThePendric <hendrik@dhis2.org>
2026-02-02 12:20:33 +0100

    chore: conditionally show the search input in the sidebar and add wrapper

--- src/components/main-sidebar/main-sidebar.tsx
+++ src/components/main-sidebar/main-sidebar.tsx
@@ -3,10 +3,13 @@ import { DataSourceSelect } from './data-source-select/data-source-select'
 import classes from './styles/main-sidebar.module.css'
 import { UnifiedSearchInput } from './unified-search-input'
 import { useAppSelector } from '@hooks'
+import { getHasDataSource } from '@store/dimensions-selection-slice'
 import { getUiMainSidebarVisible } from '@store/ui-slice'
 
 export const MainSidebar = () => {
     const isMainSidebarVisible = useAppSelector(getUiMainSidebarVisible)
+    const hasDataSource = useAppSelector(getHasDataSource)
+
     return (
         <div
             className={cx(classes.container, {
@@ -14,7 +17,11 @@ export const MainSidebar = () => {
             })}
         >
             <DataSourceSelect />
-            <UnifiedSearchInput />
+            {hasDataSource && (
+                <div className={classes.searchRow}>
+                    <UnifiedSearchInput />
+                </div>
+            )}
         </div>
     )
 }
