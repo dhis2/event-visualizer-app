ffeb63b5690f11403465ced7010ae5cd1d0e992d
Edoardo Sabadelli <edoardo@dhis2.org>
2026-02-16 14:08:01 +0100

    fix: address responsiveness for action buttons in Toolbar

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -5,16 +5,19 @@ import { MenuBar } from './menu-bar/menu-bar'
 import { NewButton } from './new-button'
 import { OpenButton } from './open-button'
 import { SaveButton } from './save-button'
+import classes from './styles/toolbar.module.css'
 import { ToolbarDivider } from './toolbar-divider'
 import { TitleBar } from '@components/title-bar/title-bar'
 import { Toolbar as AnalyticsToolbar } from '@dhis2/analytics'
 
 export const Toolbar: FC = () => (
     <AnalyticsToolbar>
-        <NewButton />
-        <OpenButton />
-        <SaveButton />
-        <ToolbarDivider />
+        <div className={classes.actionButtons}>
+            <NewButton />
+            <OpenButton />
+            <SaveButton />
+            <ToolbarDivider />
+        </div>
         <MenuBar />
         <ToolbarDivider />
         <TitleBar />
