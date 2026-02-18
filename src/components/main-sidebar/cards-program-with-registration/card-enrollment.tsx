95d31b91af2b83dd0892b3f53cb8e58328d37fe1
Hendrik de Graaf <hendrik@dhis2.org>
2026-02-18 14:13:06 +0100

    chore: implement enrollment card

--- src/components/main-sidebar/cards-program-with-registration/card-enrollment.tsx
+++ src/components/main-sidebar/cards-program-with-registration/card-enrollment.tsx
@@ -1,31 +1,64 @@
+import i18n from '@dhis2/d2-i18n'
+import { useMemo } from 'react'
 import {
     DimensionCard,
     DimensionList,
-    DimensionListItem,
 } from '@components/main-sidebar/dimension-card'
-import type { DataSourceProgramWithRegistration } from '@types'
+import { useDimensionList } from '@components/main-sidebar/use-dimension-list'
+import type {
+    DataSourceProgramWithRegistration,
+    DimensionMetadataItem,
+} from '@types'
 
 type CardEnrollmentProps = {
     program: DataSourceProgramWithRegistration
 }
 
-export const CardEnrollment = ({
-    program, // eslint-disable-line @typescript-eslint/no-unused-vars
-}: CardEnrollmentProps) => {
-    // TODO: Use custom label from program.displayEnrollmentLabel or fallback
-    const label = 'Enrollment data'
+const getFixedDimensions = (
+    program: DataSourceProgramWithRegistration
+): DimensionMetadataItem[] => {
+    return [
+        {
+            id: 'ou',
+            dimensionType: 'ORGANISATION_UNIT',
+            name: program.displayOrgUnitLabel ?? i18n.t('Enrollment org. unit'),
+            valueType: 'ORGANISATION_UNIT',
+        },
+        {
+            id: 'enrollmentDate',
+            dimensionType: 'PERIOD',
+            name:
+                program.displayEnrollmentDateLabel ??
+                i18n.t('Date of enrollment'),
+            valueType: 'DATE',
+        },
+        {
+            id: 'incidentDate',
+            dimensionType: 'PERIOD',
+            name: program.displayIncidentDateLabel ?? i18n.t('Incident date'),
+            valueType: 'DATE',
+        },
+        {
+            id: 'programStatus',
+            dimensionType: 'STATUS',
+            name: i18n.t('Enrollment status'),
+            valueType: 'TEXT',
+        },
+    ]
+}
 
+export const CardEnrollment = ({ program }: CardEnrollmentProps) => {
+    const dimensionCardKey = 'enrollment'
+    const title = program.displayEnrollmentLabel ?? i18n.t('Enrollment data')
+    const fixedDimensions = useMemo(
+        () => getFixedDimensions(program),
+        [program]
+    )
+    const listProps = useDimensionList({ fixedDimensions })
+    console.log(listProps)
     return (
-        <DimensionCard dimensionCardKey="enrollment" title={label}>
-            <DimensionList>
-                {/* TODO: Add enrollment dimensions:
-                    - Enrollment org. unit
-                    - Enrollment period(s)
-                    - Enrollment status (program status)
-                */}
-                <DimensionListItem />
-                <DimensionListItem />
-            </DimensionList>
+        <DimensionCard dimensionCardKey={dimensionCardKey} title={title}>
+            <DimensionList {...listProps} program={program} />
         </DimensionCard>
     )
 }
