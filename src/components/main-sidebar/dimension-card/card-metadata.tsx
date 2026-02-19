e36333ef101198becb076496e3f5af9e9e58b5c6
Hendrik de Graaf <hendrik@dhis2.org>
2026-02-19 09:00:04 +0100

    chore: add card metadata

--- src/components/main-sidebar/dimension-card/card-metadata.tsx
+++ src/components/main-sidebar/dimension-card/card-metadata.tsx
@@ -1,26 +1,52 @@
+import i18n from '@dhis2/d2-i18n'
+import { useMemo } from 'react'
 import {
     DimensionCard,
     DimensionList,
-    DimensionListItem,
 } from '@components/main-sidebar/dimension-card'
+import { useDimensionList } from '@components/main-sidebar/use-dimension-list'
+import type { DimensionMetadataItem } from '@types'
 
-// Metadata dimension IDs based on specifications
-const METADATA_DIMENSIONS = [
-    'lastUpdatedOn',
-    'lastUpdatedBy',
-    'createdOn',
-    'createdBy',
-    'completedOn',
-] as const
+const getFixedDimensions = (): DimensionMetadataItem[] => [
+    {
+        id: 'lastUpdated',
+        name: i18n.t('Last updated on'),
+        dimensionType: 'PERIOD',
+        valueType: 'DATE',
+    },
+    {
+        id: 'lastUpdatedBy',
+        name: i18n.t('Last updated by'),
+        dimensionType: 'USER',
+        valueType: 'USERNAME',
+    },
+    {
+        id: 'created',
+        name: i18n.t('Created on'),
+        dimensionType: 'PERIOD',
+        valueType: 'DATE',
+    },
+    {
+        id: 'createdBy',
+        name: i18n.t('Created by'),
+        dimensionType: 'USER',
+        valueType: 'USERNAME',
+    },
+    {
+        id: 'completed',
+        name: i18n.t('Completed on'),
+        dimensionType: 'PERIOD',
+        valueType: 'DATE',
+    },
+]
 
 export const CardMetadata = () => {
+    const fixedDimensions = useMemo(getFixedDimensions, [])
+    const listProps = useDimensionList({ fixedDimensions })
+
     return (
-        <DimensionCard dimensionCardKey="metadata" title="Metadata">
-            <DimensionList>
-                {METADATA_DIMENSIONS.map((dimensionId) => (
-                    <DimensionListItem key={dimensionId} />
-                ))}
-            </DimensionList>
+        <DimensionCard dimensionCardKey="metadata" title={i18n.t('Metadata')}>
+            <DimensionList {...listProps} />
         </DimensionCard>
     )
 }
