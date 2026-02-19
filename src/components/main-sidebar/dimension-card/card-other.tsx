3e5a9b61021205c2c0ef6f6662c8fa9bdd494fba
Hendrik de Graaf <hendrik@dhis2.org>
2026-02-19 08:59:39 +0100

    chore: add card other

--- src/components/main-sidebar/dimension-card/card-other.tsx
+++ src/components/main-sidebar/dimension-card/card-other.tsx
@@ -1,24 +1,38 @@
+import i18n from '@dhis2/d2-i18n'
 import {
     DimensionCard,
     DimensionList,
-    DimensionListItem,
 } from '@components/main-sidebar/dimension-card'
+import { useDimensionList } from '@components/main-sidebar/use-dimension-list'
+import type { SingleQuery } from '@types'
+
+const CARD_AND_LIST_KEY = 'other'
+const BASE_QUERY: SingleQuery = {
+    resource: 'dimensions',
+    params: {
+        pageSize: 10,
+        fields: 'id,dimensionType,valueType,optionSet,displayName~rename(name)',
+        filter: 'dimensionType:eq:ORGANISATION_UNIT_GROUP_SET',
+        order: 'displayName:asc',
+    },
+}
 
 export const CardOther = () => {
-    // TODO: Fetch "Other" dimensions (Your dimensions)
-    // TODO: Hide card if no dimensions to display
-    const otherDimensions: string[] = []
+    const { dimensions, ...listProps } = useDimensionList({
+        dimensionListKey: CARD_AND_LIST_KEY,
+        baseQuery: BASE_QUERY,
+    })
 
-    if (otherDimensions.length === 0) {
+    if (dimensions.length === 0) {
         return null
     }
 
     return (
-        <DimensionCard dimensionCardKey="other" title="Other">
-            <DimensionList>
-                <DimensionListItem />
-                <DimensionListItem />
-            </DimensionList>
+        <DimensionCard
+            dimensionCardKey={CARD_AND_LIST_KEY}
+            title={i18n.t('Other')}
+        >
+            <DimensionList {...listProps} dimensions={dimensions} />
         </DimensionCard>
     )
 }
