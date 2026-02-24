138b05018acb7ec01c514f2981976021b12e6a49
Edoardo Sabadelli <edoardo@dhis2.org>
2026-02-24 10:10:50 +0100

    chore: remove className prop and use styles directly in components

--- src/components/toolbar/actions-bar/download-menu.tsx
+++ src/components/toolbar/actions-bar/download-menu.tsx
@@ -1,21 +1,22 @@
 import i18n from '@dhis2/d2-i18n'
+import cx from 'classnames'
 import type { FC } from 'react'
+import classes from './styles/actions-bar.module.css'
 import { DownloadMenu as BaseDownloadMenu } from '@components/download-menu/download-menu'
 import { useDownload } from '@components/download-menu/use-download'
 import { HoverMenuDropdown } from '@dhis2/analytics'
 
-export const DownloadMenu: FC<{ className?: string }> = ({ className }) => {
+export const DownloadMenu: FC = () => {
     const { isDownloadDisabled, download } = useDownload()
 
     return (
         <HoverMenuDropdown
             label={i18n.t('Download')}
             disabled={isDownloadDisabled}
-            className={
-                className
-                    ? `push-analytics-download-dropdown-menu-button ${className}`
-                    : 'push-analytics-download-dropdown-menu-button'
-            }
+            className={cx(
+                classes.menuDropdown,
+                'push-analytics-download-dropdown-menu-button'
+            )}
         >
             <BaseDownloadMenu download={download} hoverable />
         </HoverMenuDropdown>
