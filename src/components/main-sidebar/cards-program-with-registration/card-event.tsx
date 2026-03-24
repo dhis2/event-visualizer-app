a7f922e0605c1e48663cce5e86b9d737350cce01
Hendrik de Graaf <hendrik@dhis2.org>
2026-03-24 16:07:54 +0100

    chore: add total selection count to all cards apart from card-type

--- src/components/main-sidebar/cards-program-with-registration/card-event.tsx
+++ src/components/main-sidebar/cards-program-with-registration/card-event.tsx
@@ -1,17 +1,26 @@
 import i18n from '@dhis2/d2-i18n'
-import { type FC, useMemo } from 'react'
+import { type FC, useCallback, useMemo } from 'react'
 import { ProgramStageSubsection } from './program-stage-subsection'
 import { DimensionCard } from '@components/main-sidebar/dimension-card'
 import { EVENT_WITH_REGISTRATION_FIXED_DIMENSION_TYPES } from '@components/main-sidebar/get-event-fixed-dimensions'
 import { computeIsDisabledByFilter } from '@components/main-sidebar/use-dimension-list'
 import { getDataElementQueryTemplate } from '@components/main-sidebar/use-dimension-list/query-helpers'
+import {
+    useSelectedDimensionCount,
+    type UseSelectedDimensionCountMatchFn,
+} from '@components/main-sidebar/use-selected-dimension-count'
 import { useAppSelector, useCurrentUser } from '@hooks'
+import { isPopulatedString } from '@modules/validation'
 import { getFilter } from '@store/dimensions-selection-slice'
-import type { DataSourceProgramWithRegistration } from '@types'
+import type { DataSourceProgramWithRegistration, DimensionType } from '@types'
 
 type CardEventProps = {
     program: DataSourceProgramWithRegistration
 }
+
+const EVENT_DIMENSION_TYPES = new Set<DimensionType>(
+    EVENT_WITH_REGISTRATION_FIXED_DIMENSION_TYPES
+).add('DATA_ELEMENT')
 
 export const CardEvent: FC<CardEventProps> = ({ program }) => {
     const {
@@ -31,12 +40,28 @@ export const CardEvent: FC<CardEventProps> = ({ program }) => {
             ),
         [stageQueryWithoutStageId, filter]
     )
+    const stageIdLookup = useMemo(
+        () =>
+            new Set(
+                program.programStages!.map((programStage) => programStage.id)
+            ),
+        [program.programStages]
+    )
+    const isSelectedMatchFn: UseSelectedDimensionCountMatchFn = useCallback(
+        (dimension) =>
+            EVENT_DIMENSION_TYPES.has(dimension.dimensionType) &&
+            isPopulatedString(dimension.programStageId) &&
+            stageIdLookup.has(dimension.programStageId),
+        [stageIdLookup]
+    )
+    const selectedCount = useSelectedDimensionCount(isSelectedMatchFn)
     return (
         <DimensionCard
             dimensionCardKey="event-with-registration"
             title={program.displayEventLabel ?? i18n.t('Event data')}
             withSubSections
             isDisabledByFilter={isDisabledByFilter}
+            selectedCount={selectedCount}
         >
             {program.programStages!.map((stage) => (
                 <ProgramStageSubsection
