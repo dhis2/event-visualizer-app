d624394d965e6067d9cac0f7e929f32de74387fe
Edoardo Sabadelli <edoardo@dhis2.org>
2026-05-22 14:35:44 +0200

    feat: implement download for PT (WIP)

--- src/components/download-menu/use-download.ts
+++ src/components/download-menu/use-download.ts
@@ -159,12 +159,137 @@ const useDownload: (relativePeriodDate?: string) => UseDownloadResult = (
         ]
     )
 
+    const downloadForPT: DownloadFn = useCallback(
+        (type, format, idScheme) => {
+            if (isVisualizationEmpty(currentVis)) {
+                return
+            }
+
+            let target = '_top'
+
+            const visualization =
+                transformVisualizationForAnalyticsRequest(currentVis)
+
+            const { adaptedVisualization, parameters } =
+                getAdaptedVisualization(visualization)
+
+            let req = new analyticsEngine.request()
+                .withPath(
+                    `${getAnalyticsEndpoint(visualization.outputType)}/aggregate`
+                )
+                .withFormat(format)
+                .withDisplayProperty(currentUser.settings.displayProperty)
+                .withProgram(
+                    getSingleProgramFromVisualization(visualization).id
+                )
+
+            // TODO: not in CurrentVisualization
+            //            if (visualization.programStatus) {
+            //                req = req.withProgramStatus(visualization.programStatus)
+            //            }
+
+            if (visualization.sortOrder) {
+                req = req.withSortOrder(
+                    visualization.sortOrder === 1 ? 'ASC' : 'DESC'
+                )
+            }
+
+            if (visualization.topLimit) {
+                req = req.withLimit(visualization.topLimit)
+            }
+
+            // add custom value and aggregationType
+            if (visualization.value && visualization.aggregationType) {
+                req = req
+                    .withValue(visualization.value.id)
+                    .withAggregationType(visualization.aggregationType)
+            }
+
+            if (
+                relativePeriodDate &&
+                isVisualizationWithTimeDimension(visualization)
+            ) {
+                req = req.withRelativePeriodDate(relativePeriodDate)
+            }
+
+            switch (type) {
+                case 'table':
+                    req = req
+                        .fromVisualization(adaptedVisualization)
+                        .withTableLayout()
+                        .withColumns(
+                            visualization.columns
+                                ?.map((column) => column.dimension)
+                                .join(';')
+                        )
+                        .withRows(
+                            visualization.rows
+                                ?.map((row) => row.dimension)
+                                .join(';')
+                        )
+                        .withParameters({
+                            ...parameters,
+                            dataIdScheme: 'NAME',
+                            paging: false,
+                        })
+
+                    target = format === 'html+css' ? '_blank' : '_top'
+
+                    break
+                case 'plain':
+                    req = req
+                        // Perhaps the 2nd arg `passFilterAsDimension` should be false for the advanced submenu?
+                        // XXX: here we don't get all dimensions since the ones without a condition are skipped
+                        .fromVisualization(adaptedVisualization, true)
+                        .withParameters({
+                            ...parameters,
+                            paging: false,
+                        })
+
+                    // fix option set option names
+                    if (idScheme === 'NAME') {
+                        req = req.withParameters({
+                            dataIdScheme: idScheme,
+                        })
+                    } else {
+                        req = req.withOutputIdScheme(idScheme)
+                    }
+
+                    target = ['csv', 'xsl', 'xslx'].includes(format)
+                        ? '_top'
+                        : '_blank'
+                    break
+            }
+
+            const url = new URL(
+                `${config.baseUrl}/api/${config.apiVersion}/${req.buildUrl()}`,
+                `${window.location.origin}${window.location.pathname}`
+            )
+
+            Object.entries(req.buildQuery()).forEach(([key, value]) =>
+                // TODO: buildQuery return value should be typed to avoid the need of the String casting
+                url.searchParams.append(key, String(value))
+            )
+
+            window.open(url, target)
+        },
+        [
+            analyticsEngine,
+            config.baseUrl,
+            config.apiVersion,
+            currentVis,
+            currentUser.settings,
+            relativePeriodDate,
+        ]
+    )
+
     // XXX: should the transformed visualization be passed here?
     const isDownloadEnabled = isVisualizationValid(currentVis)
 
     return {
         isDownloadDisabled: !isDownloadEnabled,
-        download: downloadForLL,
+        download:
+            currentVis.type === 'PIVOT_TABLE' ? downloadForPT : downloadForLL,
     }
 }
 
